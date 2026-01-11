2b91e4d335a88f8845a2731d921ee35fc77b5963
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:49:34 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "elastic_cloud_elasticsearchs" {
+  description = "All elastic_cloud_elasticsearch resources"
+  value       = azurerm_elastic_cloud_elasticsearch.elastic_cloud_elasticsearchs
+}
+
---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "elastic_cloud_elasticsearchs" {
+  description = <<EOT
+Map of elastic_cloud_elasticsearchs, attributes below
+Required:
+    - elastic_cloud_email_address
+    - location
+    - name
+    - resource_group_name
+    - sku_name
+Optional:
+    - monitoring_enabled
+    - tags
+    - logs (block):
+        - filtering_tag (optional, block):
+            - action (required)
+            - name (required)
+            - value (required)
+        - send_activity_logs (optional)
+        - send_azuread_logs (optional)
+        - send_subscription_logs (optional)
+EOT
+
+  type = map(object({
+    elastic_cloud_email_address = string
+    location                    = string
+    name                        = string
+    resource_group_name         = string
+    sku_name                    = string
+    monitoring_enabled          = optional(bool, true)
+    tags                        = optional(map(string))
+    logs = optional(object({
+      filtering_tag = optional(object({
+        action = string
+        name   = string
+        value  = string
+      }))
+      send_activity_logs     = optional(bool, false)
+      send_azuread_logs      = optional(bool, false)
+      send_subscription_logs = optional(bool, false)
+    }))
+  }))
+}
+
